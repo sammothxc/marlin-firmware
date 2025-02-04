b8a1f861a30b4ad026dd39c0794974f6c314ed7b
Sam Warr <sdwarrior8@gmail.com>
2025-02-04 14:51:08 -0700

    Update machine name in configuration to "Spaghetti Monster"

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Spaghetti Monster II"
+#define CUSTOM_MACHINE_NAME "Spaghetti Monster"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
