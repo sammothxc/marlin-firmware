3123ecc07a2beb3defd2486489143f7035e331d3
Sam Warr <sdwarrior8@gmail.com>
2025-02-04 14:51:59 -0700

    🛠️ Update custom machine name to "Printcess Bride"

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Printimus Prime"
+#define CUSTOM_MACHINE_NAME "Printcess Bride"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
